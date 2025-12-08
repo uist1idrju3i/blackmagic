0382fb3d2cc468e53621e42a9157167b49ebd47b
dragonmux <git@dragonmux.network>
2025-12-08 10:39:59 +0000

    bmp-v3/usb: Implemented endpoint reset and setup

--- src/platforms/bmp-v3/usb.c
+++ src/platforms/bmp-v3/usb.c
@@ -58,7 +58,7 @@ static uint16_t bmd_dwc2_write_packet(usbd_device *device, uint8_t endpoint, con
 static void bmd_dwc2_stall_set(usbd_device *device, uint8_t endpoint, uint8_t stall);
 static uint8_t bmd_dwc2_stall_get(usbd_device *device, uint8_t endpoint);
 static void bmd_dwc2_nak_set(usbd_device *device, uint8_t endpoint, uint8_t nak);
-static void bmd_dwc2_ep_setup(usbd_device *device, uint8_t endpoint_address, uint8_t type, uint16_t max_packet_length,
+static void bmd_dwc2_setup(usbd_device *device, uint8_t endpoint_address, uint8_t type, uint16_t max_packet_length,
 	void (*callback)(usbd_device *usbd_dev, uint8_t ep));
 static void bmd_dwc2_endpoints_reset(usbd_device *device);
 static void bmd_dwc2_poll(usbd_device *device);
@@ -68,7 +68,7 @@ static usbd_device usbd_dev;
 const struct _usbd_driver bmd_dwc2_usb_driver = {
 	.init = bmd_dwc2_init,
 	.set_address = bmd_dwc2_set_address,
-	.ep_setup = bmd_dwc2_ep_setup,
+	.ep_setup = bmd_dwc2_setup,
 	.ep_reset = bmd_dwc2_endpoints_reset,
 	.ep_stall_set = bmd_dwc2_stall_set,
 	.ep_stall_get = bmd_dwc2_stall_get,
@@ -113,8 +113,8 @@ static usbd_device *bmd_dwc2_init(void)
 	OTG_FS_DAINTMSK = 0x003f003fU;
 	/* Interrupt when IN transfer has completed */
 	OTG_FS_DIEPMSK = OTG_DIEPMSK_XFRCM;
-	/* Interrupt when OUT transfer has completed SETUP phase */
-	OTG_FS_DOEPMSK = OTG_DOEPMSK_STUPM;
+	/* Interrupt when OUT transfer has completed SETUP phase or a transfer complets */
+	OTG_FS_DOEPMSK = OTG_DOEPMSK_STUPM | OTG_DOEPMSK_XFRCM;
 
 	/* Enable global interrupts now we're all set */
 	OTG_FS_GAHBCFG |= OTG_GAHBCFG_GINT;
@@ -187,13 +187,91 @@ static void bmd_dwc2_nak_set(usbd_device *const device, const uint8_t endpoint, 
 {
 }
 
-static void bmd_dwc2_ep_setup(usbd_device *const device, const uint8_t endpoint_address, const uint8_t type,
+static void bmd_dwc2_setup(usbd_device *const device, const uint8_t endpoint_address, const uint8_t type,
 	const uint16_t max_packet_length, void (*const callback)(usbd_device *usbd_dev, uint8_t ep))
 {
+	const uint8_t ep = endpoint_address & 0x7fU;
+	const uint8_t dir = endpoint_address & 0x80U;
+	/* Convert the max packet length to a length in u32's */
+	const uint16_t packet_length = max_packet_length / 4U;
+
+	/* Process if we're being asked to set up EP0, */
+	if (ep == 0U) {
+		/* Start by setting up the TX and RX FIFOs */
+		OTG_FS_GRXFSIZ = device->driver->rx_fifo_size;
+		OTG_FS_GNPTXFSIZ = (packet_length << 16U) | device->driver->rx_fifo_size;
+		/* Update our internal state for how the FIFOs are presently allocated */
+		device->fifo_mem_top_ep0 = device->driver->rx_fifo_size + packet_length;
+		device->fifo_mem_top = device->fifo_mem_top_ep0;
+
+		/* Configure EP0 IN to allow us to send packets appropriately */
+		if (max_packet_length >= 64U)
+			OTG_FS_DIEPCTL0 = OTG_DIEPCTL0_MPSIZ_64;
+		else if (max_packet_length >= 32U)
+			OTG_FS_DIEPCTL0 = OTG_DIEPCTL0_MPSIZ_32;
+		else if (max_packet_length >= 16U)
+			OTG_FS_DIEPCTL0 = OTG_DIEPCTL0_MPSIZ_16;
+		else
+			OTG_FS_DIEPCTL0 = OTG_DIEPCTL0_MPSIZ_8;
+
+		/* Now configure EP0 OUT to allow us to receive SETUP packets */
+		device->doeptsiz[0U] =
+			OTG_DOEPSIZ0_STUPCNT_3 | OTG_DOEPSIZ0_PKTCNT | (max_packet_length & OTG_DOEPSIZ0_XFRSIZ_MASK);
+		OTG_FS_DOEPTSIZ0 = device->doeptsiz[0U];
+		/* Arm the endpoint to recieve packets */
+		OTG_FS_DOEPCTL0 = OTG_DOEPCTL0_EPENA | OTG_DOEPCTL0_SNAK;
+	} else {
+		/* Otherwise process if this is for IN vs OUT */
+		if (dir == 0U) {
+			/* Set up this OUT endpoint, arming it so we can get data from it */
+			device->doeptsiz[ep] = OTG_DOEPSIZX_PKTCNT(1U) | (max_packet_length & OTG_DOEPSIZX_XFRSIZ_MASK);
+			OTG_FS_DOEPTSIZ(ep) = device->doeptsiz[ep];
+			OTG_FS_DOEPCTL(ep) = OTG_DOEPCTL0_EPENA | OTG_DOEPCTL0_CNAK | OTG_DOEPCTL0_USBAEP | OTG_DOEPCTLX_SD0PID |
+				(type << OTG_DOEPCTLX_EPTYP_SHIFT) | (max_packet_length & OTG_DOEPCTLX_MPSIZ_MASK);
+
+			/* Install the user's callback if provided */
+			device->user_callback_ctr[ep][USB_TRANSACTION_OUT] = callback;
+		} else {
+			/* Set up this IN endpoint, allocating space for it in the FIFO memory */
+			OTG_FS_DIEPTXF(ep) = (packet_length << 16U) | device->fifo_mem_top;
+			device->fifo_mem_top += packet_length;
+			OTG_FS_DIEPTSIZ(ep) = 0U;
+			/* Enable the endpoint but do not yet arm it as we've not yet got anything to send */
+			OTG_FS_DIEPCTL(ep) = OTG_DIEPCTL0_SNAK | OTG_DIEPCTL0_USBAEP | OTG_DIEPCTLX_SD0PID |
+				(ep << OTG_DIEPCTLX_TXFNUM_SHIFT) | (type << OTG_DIEPCTLX_EPTYP_SHIFT) |
+				(max_packet_length & OTG_DIEPCTLX_MPSIZ_MASK);
+
+			/* Install the user's callback if provided */
+			device->user_callback_ctr[ep][USB_TRANSACTION_IN] = callback;
+		}
+	}
 }
 
 static void bmd_dwc2_endpoints_reset(usbd_device *const device)
 {
+	/* Start by resetting our FIFO setup state */
+	device->fifo_mem_top = device->fifo_mem_top_ep0;
+
+	/*
+	 * Now loop through all endpoints and make sure we're NAK'ing and they're properly disabled
+	 *
+	 * NB: We ignore EP0 here because that's handled by the EP setup call _usbd_reset() performs.
+	 */
+	for (size_t i = 1U; i < ENDPOINT_COUNT; ++i) {
+		OTG_FS_DOEPCTL(i) = OTG_DOEPCTL0_SNAK;
+		if (OTG_FS_DOEPCTL(i) & OTG_DOEPCTL0_EPENA)
+			OTG_FS_DOEPCTL(i) |= OTG_DOEPCTL0_EPDIS;
+		if (OTG_FS_DIEPCTL(i) & OTG_DIEPCTL0_EPENA)
+			OTG_FS_DIEPCTL(i) |= OTG_DIEPCTL0_EPDIS;
+	}
+
+	/* Make sure all FIFOs are fully flushed */
+	OTG_FS_GRSTCTL = OTG_GRSTCTL_TXFNUM_ALL | OTG_GRSTCTL_TXFFLSH | OTG_GRSTCTL_RXFFLSH;
+	/* Wait for that to complete */
+	while ((OTG_FS_GRSTCTL & (OTG_GRSTCTL_TXFFLSH | OTG_GRSTCTL_RXFFLSH)) != 0U)
+		continue;
+	/* Reset the GRSTCTL register state */
+	OTG_FS_GRSTCTL &= ~OTG_GRSTCTL_TXFNUM_MASK;
 }
 
 static void bmd_dwc2_poll(usbd_device *const device)
@@ -202,7 +280,7 @@ static void bmd_dwc2_poll(usbd_device *const device)
 	/* First check to see if we're here for a USB reset event */
 	if (status & OTG_GINTSTS_USBRST) {
 		/* Do an endpoint reset, make sure EP0 is set up, and clear the condition */
-		device->fifo_mem_top = device->driver->rx_fifo_size;
+		bmd_dwc2_endpoints_reset(device);
 		_usbd_reset(device);
 		OTG_FS_GINTSTS = OTG_GINTSTS_USBRST;
 		/* Exit early as we're done here */
